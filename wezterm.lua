0ddcae0e29d2fc3c79a2c33c5468f041704834c0
Peter Sparks <psparks1992@gmail.com>
2025-01-14 16:34:38 -0600

    setting up right click copy/paste behavior

--- wezterm.lua
+++ wezterm.lua
@@ -141,6 +141,22 @@ config.keys = {
 	},
 }
 
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
+
 if wezterm.target_triple:find("windows") then
 	config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" }
 end
